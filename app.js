67c91c0ad696c0731b03978b2ecb178a7f96158a
owaissikander <154621974+owaissikander@users.noreply.github.com>
2024-06-19 14:09:12 +0500

    incomplete

--- app.js
+++ app.js
@@ -2,17 +2,32 @@
 
 const addbutton = document.querySelector('#addbutton')
 const main = document.querySelector('#main')
+// const userSearch=document.getElementById('search')
+// ye pura kaam baat han jo phey nhi karna 
+
+
+// userSearch.addEventListener('change', function(){
+//     var findOut=
+// })
 
 //const saveNotes = () =>
 function saveNotes() {
-    const notes = document.querySelectorAll('.note textarea');
-
+    const notes = document.querySelectorAll('.note-textarea');
+    const untitled = document.querySelectorAll('#untitled')
     const data = [];
+    const untitled_data = []
+    untitled.forEach((untitled) => {
+        untitled_data.push(untitled.value)
+    })
+    console.log(untitled_data)
+    localStorage.setItem('untitled', JSON.stringify(untitled_data))
+
+    
     notes.forEach(
         (note) => {
             data.push(note.value)
         })
-    //console.log(data)
+    console.log(data)
     localStorage.setItem('notes', JSON.stringify(data))
 }
 
@@ -22,19 +37,20 @@ addbutton.addEventListener('click', function () {
 
 
 //const notes_add = () => 
-function notes_add() {
+function notes_add(text = '', untitled_text = '') {
     const note = document.createElement('div');
-    note.classList.add("note")
+    note.classList.add("note",'untitled')
     note.innerHTML = `
      <div class="note">
             
           <div class="note">
             
             <div class="tools">
+           <textarea id="untitled" class="untitled"  placeholder="untitled">${untitled_text}</textarea>
                 <i class="save fa-solid fa-floppy-disk"></i>
                 <i class="trash fa-solid fa-trash"></i>
             </div>
-            <textarea></textarea>
+            <textarea id="content">${text}</textarea>
         </div>
     `
     main.appendChild(note);
@@ -44,6 +60,7 @@ function notes_add() {
         'click',
         function () {
             note.remove()
+            saveNotes()
         }
     )
     note.querySelector('.save').addEventListener('click', function () {
@@ -51,3 +68,17 @@ function notes_add() {
     })
     main.appendChild(note);
 }
+
+
+(
+    function () {
+        const lsnote = JSON.parse(localStorage.getItem("notes"))
+        //console.log(lsnote)
+        lsnote.forEach((lsnote) => {
+            notes_add(lsnote)
+            saveNotes()
+        })
+    }
+
+
+)()
